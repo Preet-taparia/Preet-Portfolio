9c57707bafb23f7073722a1c0ccac3c28d87f6b7
Preet Taparia <preettaparia@gmail.com>
2024-08-17 14:09:52 +0530

    update heading

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -40,72 +40,77 @@ const Contact: React.FC = () => {
         email: "",
         message: "",
       });
-    }, 1000); 
+    }, 1000);
   };
 
   return (
 
-    <div id="contact">
-      <Header startLine="A small selection of" endLine="recent projects" />
-
-    <div className={`flex xl:flex-row flex-col-reverse gap-10 pt-10`}>
-      <div
-        className='bg-black-100 p-8 rounded-2xl'
-      >
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className='flex flex-col gap-4'
+    <div className="py-10" id="contact">
+      <Header startLine="Let's get in" endLine="Touch" />
+      <div className={`flex xl:flex-row flex-col-reverse gap-10 pt-10`}>
+        <div
+          className='bg-black-100 p-8 rounded-2xl'
         >
-          <label className='flex flex-col'>
-            <span className='text-white font-medium'>Your Name</span>
-            <input
-              type='text'
-              name='name'
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium'>Your email</span>
-            <input
-              type='email'
-              name='email'
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your web address?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium'>Your Message</span>
-            <textarea
-              rows={4}
-              name='message'
-              value={form.message}
-              onChange={handleChange}
-              placeholder='What you want to say?'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-
-          <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className='flex flex-col gap-4'
           >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
-      </div>
+            <label className='flex flex-col'>
+              <span className='text-white font-medium'>Your Name</span>
+              <input
+                type='text'
+                name='name'
+                value={form.name}
+                onChange={handleChange}
+                placeholder="What's your good name?"
+                className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              />
+            </label>
+            <label className='flex flex-col'>
+              <span className='text-white font-medium'>Your email</span>
+              <input
+                type='email'
+                name='email'
+                value={form.email}
+                onChange={handleChange}
+                placeholder="What's your web address?"
+                className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              />
+            </label>
+            <label className='flex flex-col'>
+              <span className='text-white font-medium'>Your Message</span>
+              <textarea
+                rows={4}
+                name='message'
+                value={form.message}
+                onChange={handleChange}
+                placeholder='What you want to say?'
+                className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              />
+            </label>
 
-      <div
-        className= 'w-[350px] h-[350px] md:w-[550px] md:h-[550px] lg:w-[650px] lg:h-[650px] mx-auto  '
-      >
-        <EarthCanvas />
+            <button
+              className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
+              type='submit'
+            >
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span
+                className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+             bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 `}
+              >
+                {loading ? "Sending..." : "Send"}
+              </span>
+            </button>
+          </form>
+        </div>
+
+        <div
+          className='w-[350px] h-[350px] md:w-[550px] md:h-[550px] lg:w-[650px] lg:h-[650px] mx-auto  '
+        >
+          <EarthCanvas />
+        </div>
       </div>
-    </div>
     </div>
   );
 };
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 const animateClass = 'hover:animate-rubberBand';
 
 const splitText = (text: string) => {
-  return text.split('').map((char, index) => (
-    <span
-      key={index}
-      className={`inline-block ${animateClass}`}
-      style={{ display: char === ' ' ? 'inline' : 'inline-block' }} // Ensure spaces are inline
-    >
-      {char}
+  return text.split(' ').map((word, index) => (
+    <span key={index} style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>
+      {word.split('').map((char, charIndex) => (
+        <span key={charIndex} className={`inline-block ${animateClass}`}>
+          {char}
+        </span>
+      ))}
+      <span>&nbsp;</span>
     </span>
   ));
 };
@@ -17,7 +18,7 @@ const splitText = (text: string) => {
 const Header = ({ startLine, endLine }: { startLine: string, endLine: string }) => {
   return (
     <h1 className="heading py-10">
-      {splitText(startLine)}{' '}
+      {splitText(startLine)}
       <span className="text-purple">
         {splitText(endLine)}
       </span>
