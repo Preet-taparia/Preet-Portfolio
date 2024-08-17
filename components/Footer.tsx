726dd50b248e4b5d3b04e8756d1144c561811fe3
Preet Taparia <preettaparia@gmail.com>
2024-08-17 22:15:36 +0530

    update key footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"; // Import FontAwesome icons
-import { SiCodechef, SiLeetcode, SiGeeksforgeeks } from "react-icons/si"; // Import react-icons
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { SiCodechef, SiLeetcode, SiGeeksforgeeks } from "react-icons/si";
 import { socialMedia } from "@/data";
 
 const Footer = () => {
@@ -29,18 +29,19 @@ const Footer = () => {
           Made by Preet Taparia
         </p>
 
-        <div className="flex items-center mt-5 sm:mt-0 gap-1 sm:gap-2 md:gap-3 ">
+        <div className="flex items-center mt-5 sm:mt-0 gap-1 sm:gap-2 md:gap-3">
           {socialMedia.map((info) => (
             <div
+              key={info.id}
               className="relative inline-flex overflow-hidden mx-auto rounded-lg p-[1px] focus:outline-none"
             >
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
               <span className="h-8 w-8 sm:h-10 sm:w-10 inline-flex cursor-pointer items-center justify-center rounded-lg bg-slate-950 text-sm font-medium text-white backdrop-blur-3xl gap-2">
                 <a
-                  key={info.id}
                   href={info.link}
                   target="_blank"
-                  rel="noopener noreferrer"                >
+                  rel="noopener noreferrer"
+                >
                   {getIcon(info.icon)}
                 </a>
               </span>
